06c347304918e5ecb35db95f2f9c3301021ab796
wonone <wonone0902@gmail.com>
2021-11-16 17:17:19 +0900

    greey

---
+++ greedy.py
@@ -0,0 +1,17 @@
+a= input()
+
+x = int(ord(a[0])) - int(ord(a[0]))+1
+y = int(a[1])
+count =0
+move = [(2,1), (-2,1), (2,-1), (1,2), (-1, 2), (1, -2), (-1, -2), (-2, -1)]
+
+for i in range(len(move)):
+  dx = x + move[i][0]
+  dy = y + move[i][1]
+
+  if dx >=1 and dx <=8 and dy <8 and dy >= 1:
+    count +=1
+5  else:
+    continue
+
+print(count)
